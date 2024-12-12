ba16a9625b4812b90c794c7cca3b2ccb29b2613e
siteswap <siteswap@github.com>
2024-12-12 13:24:16 +0000

    Tease apart the plasmaBlast and mobiusRing dependencies

--- static/js/PhysicsController.js
+++ static/js/PhysicsController.js
@@ -1,5 +1,4 @@
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { MovementController } from './MovementController.js';
 import { MobiusRingController } from './MobiusRingController.js';
 import { PlasmaBlastController } from './PlasmaBlastController.js';
@@ -36,7 +35,14 @@ export class PhysicsController {
         this.movementController.onKeyUp(event);
     }
 
+    isMobiusRing(object) {
+        const mobiusRings = this.mobiusRingController.getRings();
+        return mobiusRings.find(ring => ring === object || ring.children.includes(object));
+    }
+
     updatePlasmaBlasts(deltaTime, collidableMeshes) {
+        const collisions = [];
+
         for (let i = this.plasmaBlasts.length - 1; i >= 0; i--) {
             const plasma = this.plasmaBlasts[i];
             
@@ -51,32 +57,29 @@ export class PhysicsController {
                 continue;
             }
 
-            // Check for collisions with objects
+            // Get a Raycaster to check for collisions
             const plasmaRaycaster = new THREE.Raycaster(
                 plasma.position,
                 plasma.velocity.clone().normalize(),
                 0,
                 0.5
             );
+
+            // Check for collisions with objects
             const intersects = plasmaRaycaster.intersectObjects(collidableMeshes);
             if (intersects.length > 0) {
-                // Check if the intersected object is a mobius ring
                 const hitObject = intersects[0].object;
-                const mobiusRings = this.mobiusRingController.getRings();
-                const mobiusRing = mobiusRings.find(ring => ring === hitObject || ring.children.includes(hitObject));
                 
-                if (mobiusRing) {
-                    // Remove the mobius ring
-                    if (this.mobiusRingController.removeRing(mobiusRing)) {
-                        this.score += 1;
-                    }
-                }
+                // Add collision information
+                collisions.push(hitObject);
 
                 // Remove the plasma blast
                 this.scene.remove(plasma);
                 this.plasmaBlasts.splice(i, 1);
             }
         }
+
+        return collisions;
     }
 
     update(deltaTime) {
@@ -91,6 +94,17 @@ export class PhysicsController {
 
         this.mobiusRingController.updateMobiusRings(deltaTime);
         this.movementController.update(deltaTime, collidableMeshes);
-        this.updatePlasmaBlasts(deltaTime, collidableMeshes);
+        
+        const collisions = this.updatePlasmaBlasts(deltaTime, collidableMeshes);
+        
+        // Handle collisions
+        for (const collision of collisions) {
+            const mobiusRing = this.isMobiusRing(collision)
+            if (mobiusRing) {
+                if (this.mobiusRingController.removeRing(mobiusRing)) {
+                    this.score += 1;
+                }
+            }
+        }
     }
 }
